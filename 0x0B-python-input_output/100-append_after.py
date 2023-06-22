7a0a5c5ace20f1c0eb7f96962e026293671e1a34
AhmedDiaa0212 <ahmeddeyiaa0212@gmail.com>
2023-06-22 08:19:20 -0700

    task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Search and update"""
+"""
+Module for append_after method.
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """inserts a line of text to a file, after each line with a spc. string"""
-    text = ""
-    with open(filename, "r") as file:
-        for line in file:
-            text += line
-            if search_string in line:
-                text += new_string
-    with open(filename, "w") as file:
-        file.write(text)
+    '''Method for inserting text after search string.'''
+    lines = []
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        i = 0
+        while i < len(lines):
+            if search_string in lines[i]:
+                lines[i:i + 1] = [lines[i], new_string]
+                i += 1
+            i += 1
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(lines)
